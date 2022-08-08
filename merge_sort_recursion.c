4406cdad0022fe9c0ddab2548f173514a2c3b43d
ALAN.CY.YU <ALAN.CY.YU@deltaww.com>
2022-08-08 17:19:34 +0800

    add recursion method

---
+++ merge_sort_recursion.c
@@ -0,0 +1,77 @@
+#include "merge_sort_recursion.h"
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+static void SortTwoArray(int16_t* arrIn, uint16_t arrSize1, uint16_t arrSize2);
+
+void MergeSortRecursion(int16_t* arr, uint16_t arrSize)
+{
+    uint16_t size1, size2;
+
+    if (arrSize <= 1)
+        return;
+        
+    size1 = arrSize/2;
+    size2 = arrSize - size1;
+    
+    if (size2 > 1)
+    {
+        MergeSortRecursion(arr, size1);
+        MergeSortRecursion(arr+size1, size2);
+    }
+
+    SortTwoArray(arr, size1, size2);
+
+}
+
+
+static void SortTwoArray(int16_t* arrIn, uint16_t arrSize1, uint16_t arrSize2)
+{
+    int16_t arrTemp[128];
+    int16_t* p = arrTemp;
+    int16_t i1 = 0, i2 = 0;
+    int16_t* arr1 = arrIn;
+    int16_t* arr2 = arrIn + arrSize1;
+    
+    while ((i1 < arrSize1) && (i2 < arrSize2))
+    {
+        if (arr1[i1] < arr2[i2])
+        {
+            *p = arr1[i1];
+            i1++;
+        }
+        else
+        {
+            *p = arr2[i2];
+            i2++;
+        }
+        
+        p++;
+    }
+
+    if (i1 == arrSize1)
+    {
+        for ( ; i2 < arrSize2; i2++)
+        {
+            *p = arr2[i2];
+            p++;
+        }
+    }
+    else
+    {
+        for ( ; i1 < arrSize1; i1++)
+        {
+            *p = arr1[i1];
+            p++;
+        }
+    }
+
+    for (uint8_t i = 0; i < (arrSize1+arrSize2); i++)
+    {
+        arrIn[i] = arrTemp[i];
+    }
+}
+
+
---
+++ merge_sort_recursion.h
@@ -0,0 +1,8 @@
+#ifndef MERGE_SORT_RECURSION_H
+#define MERGE_SORT_RECURSION_H
+
+#include <stdint.h>
+
+void MergeSortRecursion(int16_t* arr, uint16_t arrSize);
+
+#endif
